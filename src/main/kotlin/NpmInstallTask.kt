540f2342e7b230eaefb68a705d5204165c474c36
Paul Souteyrat <paul.souteyrat@outlok.com>
2023-03-10 09:41:31 +0000

    Using exit value in npm install task

--- src/main/kotlin/NpmInstallTask.kt
+++ src/main/kotlin/NpmInstallTask.kt
@@ -1,15 +1,20 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.TaskAction
 
+@Suppress("LeakingThis")
 abstract class NpmInstallTask : DefaultTask() {
 
     companion object {
         const val NAME = "npmInstall"
     }
+
+    @get:Input
+    abstract val ignoreExitValue: Property<Boolean>
 
     @get:InputFile
     abstract val packageJson: RegularFileProperty
@@ -18,15 +23,23 @@ abstract class NpmInstallTask : DefaultTask() {
     abstract val args: ListProperty<String>
 
     init {
-        @Suppress("LeakingThis")
-        args.convention(listOf())
         description = "Install npm dependencies"
+        ignoreExitValue.convention(false)
+        args.convention(listOf())
     }
 
     @TaskAction
     fun run() {
         val workingDir = packageJson.get().asFile.parentFile
         val executor = NpmExecutor.create("install", *args.get().toTypedArray()).directory(workingDir).start()
-        executor.process.waitFor()
+        val process = executor.process
+        process.waitFor()
+        if (process.exitValue() != 0) {
+            if (!ignoreExitValue.get()) {
+                throw RuntimeException("Npm install failed with exit value ${process.exitValue()}")
+            } else {
+                logger.warn("Npm install failed with exit value ${process.exitValue()}")
+            }
+        }
     }
 }
