5d1c2c81ca344665b63c64ddb99fd4175cf9704f
Paul Souteyrat <paul.souteyrat@outlok.com>
2024-04-23 22:45:33 +0900

    Use the Destroys annotation on the node modules directory (incremental build correct annotation)

--- src/main/kotlin/NodeCleanTask.kt
+++ src/main/kotlin/NodeCleanTask.kt
@@ -1,6 +1,7 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.Destroys
 import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.getByType
 
@@ -12,6 +13,7 @@ abstract class NodeCleanTask : DefaultTask() {
         }
     }
 
+    @get:Destroys
     abstract val nodeModules: DirectoryProperty
 
     init {
